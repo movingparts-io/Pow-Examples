a99618fae41b492fb766b360e67f37dd982a0594
Robert Böhnke <robb@movingparts.io>
2022-11-24 13:26:02 +0100

    Add info header to list

--- Pow Example/ExampleList.swift
+++ Pow Example/ExampleList.swift
@@ -1,8 +1,42 @@
+import Pow
+import MessageUI
 import SwiftUI
 
 struct ExampleList: View {
     var body: some View {
         List {
+            Section {
+                VStack(alignment: .leading, spacing: 12) {
+                    Text("Pow Examples")
+                        .font(.headline)
+                        .foregroundColor(.primary)
+
+                    Text("This is the official example app for Pow, the Surprise and Delight framework for SwiftUI.")
+
+                    Text("Tap the individual examples to see the effects and transitions in action.")
+                }
+                .font(.body.leading(.loose))
+                .foregroundColor(.secondary)
+
+                Link(destination: URL(string: "https://movingparts.io/pow")!) {
+                    Label("Pow Website", systemImage: "safari")
+                }
+
+                Link(destination: URL(string: "https://github.com/movingparts-io/Pow-Examples")!) {
+                    ViewThatFits {
+                        Label("GitHub Repository for this App", systemImage: "terminal")
+                        Label("GitHub Repo for this App", systemImage: "terminal")
+                        Label("Repo for this App", systemImage: "terminal")
+                    }
+                }
+
+                if MFMailComposeViewController.canSendMail() {
+                    Link(destination: URL(string: "mailto:hello@movingparts.io")!) {
+                        Label("Support", systemImage: "envelope")
+                    }
+                }
+            }
+
             Section  {
                 JumpExample.navigationLink
                 PingExample.navigationLink
