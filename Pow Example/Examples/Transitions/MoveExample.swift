64d101fb4778a5515af6d0aac6b7c5fb94ee1f4d
Robert Böhnke <robb@movingparts.io>
2022-11-30 16:37:46 +0100

    Expose angle in Move example

--- Pow Example/Examples/Transitions/MoveExample.swift
+++ Pow Example/Examples/Transitions/MoveExample.swift
@@ -3,22 +3,42 @@ import SwiftUI
 
 struct MoveExample: View, Example {
     @State
+    var angle: Angle = .degrees(225)
+
+    @State
     var isVisible: Bool = false
 
     var body: some View {
-        ZStack {
-            if isVisible {
-                PlaceholderView()
-                    .compositingGroup()
-                    .transition(.movingParts.move(angle: .degrees(225)).combined(with: .opacity))
+        VStack {
+            GroupBox {
+                LabeledContent {
+                    Slider(value: $angle.degrees, in: 0 ... 360, step: 5)
+                } label: {
+                    Text("Angle")
+                    Spacer()
+                    Text(Measurement(value: angle.degrees, unit: UnitAngle.degrees).formatted(.measurement(width: .narrow, numberFormatStyle: .number.precision(.fractionLength(0)))))
+                        .foregroundColor(.secondary)
+                        .font(.subheadline.monospacedDigit())
+                }
+            }
+            .padding(.horizontal)
+
+            VStack {
+                if isVisible {
+                    PlaceholderView()
+                        .compositingGroup()
+                        .transition(.movingParts.move(angle: angle).combined(with: .opacity))
+                }
+            }
+            .defaultBackground()
+            .onTapGesture {
+                withAnimation(.spring(dampingFraction: 1)) {
+                    isVisible.toggle()
+                }
             }
         }
+        .labeledContentStyle(VerticalLabeledContentStyle())
         .defaultBackground()
-        .onTapGesture {
-            withAnimation(.spring(dampingFraction: 1)) {
-                isVisible.toggle()
-            }
-        }
         .autotoggle($isVisible, with: .spring(dampingFraction: 1))
     }
 
@@ -26,5 +46,17 @@ struct MoveExample: View, Example {
     
     static var icon: Image? {
         Image(systemName: "arrow.up.left.and.down.right.and.arrow.up.right.and.down.left")
+    }
+}
+
+private struct VerticalLabeledContentStyle: LabeledContentStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .firstTextBaseline) {
+                configuration.label
+            }
+
+            configuration.content
+        }
     }
 }
